c3bf199d6c0d8b8516858968f689fe0b808905c3
kirankumar2406 <kirankumarp2406@gmail.comgit config --global user.name kirankumar2406git commit -mproject1git commit -mproject1.html>
2022-01-18 17:23:42 +0530

    final commit

---
+++ digitalclock.js
@@ -0,0 +1,90 @@
+
+
+function showTime() {
+	let time = new Date();
+	let hour = time.getHours();
+	let min = time.getMinutes();
+	let sec = time.getSeconds();
+	am_pm = "AM";
+
+	if (hour > 12) {
+		hour -= 12;
+		am_pm = "PM";
+	}
+	if (hour == 0) {
+		hour = 12;
+		am_pm = "AM";
+	}
+
+	hour = hour < 10 ? "0" + hour : hour;
+	min = min < 10 ? "0" + min : min;
+	sec = sec < 10 ? "0" + sec : sec;
+
+    document.getElementById("hours").innerHTML=`${hour} <br> hours`
+    document.getElementById("mins").innerHTML=`${min} <br> mins`
+    document.getElementById("secs").innerHTML=`${sec} <br> secs`
+    document.getElementById("am").innerHTML=am_pm
+
+    // let currentTime = hour + ":" 
+    // + min + ":" + sec + am_pm;
+	
+}
+showTime();
+setInterval(showTime, 1000);
+
+
+function newblock1(){
+
+    let a=document.createElement("div")
+    a.className="time_table"
+    a.setAttribute("id","add")
+
+    let parent=document.getElementById("dynamic")
+     parent.appendChild(a)
+
+     let invalue=document.getElementById("wake_up_time");
+     let value=invalue.options[invalue.selectedIndex].text
+
+    //  document.getElementById("add ").innerHTML=  "WAKE UP TIME : "+value 
+
+        
+     let invalue2=document.getElementById("lunch_time");
+     let value2=invalue2.options[invalue2.selectedIndex].text
+
+     let invalue3=document.getElementById("nap_time");
+     let value3=invalue3.options[invalue3.selectedIndex].text
+
+     document.getElementById("add").innerHTML=` WAKE TIME : ${value} <br><br> LUNCH TIME : ${value2} <br><br> NAP TIME : ${value3}`
+
+}   
+
+
+function settime(){
+       
+    var i=document.getElementById("wake_up_time").value
+
+    var j=document.getElementById("lunch_time").value
+
+    var k=document.getElementById("nap_time").value
+    let time=new Date().getHours();
+
+    if(i==time){
+        document.getElementById("image").style.backgroundImage="url(./wakeup_image.svg)";
+        document.getElementById("newcomment").innerHTML="Lets have some breakfast"
+    }
+    else if(j==time){
+        document.getElementById("image").style.backgroundImage="url(./lunch_image.svg)";
+        document.getElementById("newcomment").innerHTML="Lets have some lunch"
+    
+    }
+    else if(k==time){
+        document.getElementById("image").style.backgroundImage="url(./goodnight_image.svg)";
+        document.getElementById("newcomment").innerHTML="Lets sleep"
+    
+    }
+    
+
+    newblock1();
+
+}
+
